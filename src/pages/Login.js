dff51c3303731007e383ad86495ed0591bfb2dea
Jean-David EDIGHOFFER <jd.edighoffer@gmail.com>
2021-11-11 16:13:07 +0100

    erreur 400 au login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,9 +7,49 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/login",
+        { email: email, password: password }
+      ); // en fonction de la réponse
+      if (
+        response.data.password === { password } ||
+        response.data.email === { email }
+      ) {
+        Cookies.set("myToken", response.data.token, { expires: 30 });
+        navigate("/");
+      } else alert("mauvais compte ou mot de passe");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="login">
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          type="email"
+          placeholder="email"
+          value={email}
+        />
+        <br />
+        <input
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          type="password"
+          placeholder="password"
+          value={password}
+        />
+        <input type="submit" />
+      </form>
       <p>Pas encore de compte ? Inscris-toi !</p>
     </div>
   );
