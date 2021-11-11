b727047a43a6192d38f11791f0e5e127d3e12889
Jean-David EDIGHOFFER <jd.edighoffer@gmail.com>
2021-11-11 15:48:00 +0100

    signup fonctionnel

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login Component</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login">
+      <form></form>
+      <p>Pas encore de compte ? Inscris-toi !</p>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -9,48 +9,34 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [step2, setStep2] = useState(false);
-  const [cookie, setCookie] = useState();
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      // faire la requete axios
       try {
         const response = await axios.post(
-          "https://lereacteur-vinted-api.herokuapp.com/user/signup"
-        );
-        setData(response.data);
-
-        setIsLoading(false);
-
-        const handleSubmit = (event) => {
-          event.preventDefault();
-          if (password === confirmPassword) {
-            setStep2(true);
-            const token = {};
-            Cookies.set(
-              "myToken",
-              { value: response.data.token },
-              { expires: 30 }
-            );
-            navigate("/");
-          } else {
-            alert("Vos mots de passe ne sont pas identiques ! Abruti $$");
-          }
-        };
+          "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+          { username: username, email: email, password: password }
+        ); // en fonction de la réponse
+        if (response.data.token) {
+          Cookies.set("myToken", response.data.token, { expires: 30 });
+          navigate("/");
+        } else alert("erreur");
       } catch (error) {
         console.log(error.message);
       }
-    };
-    fetchData();
-  }, []);
+    } else {
+      alert("Vos mots de passe ne sont pas identiques ! Abruti $$");
+    }
+  };
 
   return (
     <div className="signup">
       <h1>S'inscrire</h1>
-      {step2 === false && (
+      {step2 === false ? (
         <form onSubmit={handleSubmit}>
           <input
             onChange={(event) => {
@@ -89,6 +75,8 @@ const Signup = () => {
           />
           <input type="submit" />
         </form>
+      ) : (
+        setStep2(true)
       )}
     </div>
   );
